bb98dd1239a7f1000deb5d23b87a9a536229b8aa
Logan Palmer <loganpalmer95@icloud.com>
2022-01-26 17:34:42 -0500

    added current day and hours and set local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,34 @@
-//var now = moment().format("dddd, MMMM Do, YYYY, h:mm");
-var now = new Date();
-$('#currentDay').text(now);
+var currentDay = moment().format("dddd, MMMM Do");
 
-var times = [
-    "09:00",
-    "10:00",
-    "11:00",
-    "12:00",
-    "13:00",
-    "14:00",
-    "15:00",
-    "16:00",
-    "17:00"
-]
+var hour9 = moment().hour(9);
+var hour10 = moment().hour(10);
+var hour11 = moment().hour(11);
+var hour12 = moment().hour(12);
+var hour13 = moment().hour(13);
+var hour14 = moment().hour(14);
+var hour15 = moment().hour(15);
+var hour16 = moment().hour(16);
+var hour17 = moment().hour(17);
+var hour18 = moment().hour(18);
 
-for (i = 0; i < times.length; i++) {
-    var time = times[i]
-    var timeColor = document.getElementById("row-" +i);
-    var timeDate = toDate(time, "h:m")
-    if (timeDate > now) {
-        timeColor.style.color = "red";
-    }
-    else {
-        timeColor.style.color = "blue";
-    }
-    if (timeDate == now) {
-        timeColor.style.color = "yellow";
-    }
-}
+var hours = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
 
-function toDate(dStr, format) {
-    var now = new Date();
-    if (format == "h:m") {
-        now.setHours(dStr.substr(0, dStr.indexOf(":")));
-        return now;
-    } else
-        return "Invalid Format";
-}
+var events9 = JSON.parse(localStorage.getItem('hour9')) || "";
+var events10 = JSON.parse(localStorage.getItem('hour10')) || "";
+var events11 = JSON.parse(localStorage.getItem('hour11')) || "";
+var events12 = JSON.parse(localStorage.getItem('hour12')) || "";
+var events13 = JSON.parse(localStorage.getItem('hour13')) || "";
+var events14 = JSON.parse(localStorage.getItem('hour14')) || "";
+var events15 = JSON.parse(localStorage.getItem('hour15')) || "";
+var events16 = JSON.parse(localStorage.getItem('hour16')) || "";
+var events17 = JSON.parse(localStorage.getItem('hour17')) || "";
+
+$.each(hours, function (index, value) {
+    events = [events9, events10, events11, events12, events13, events14, events15, events16, events17]
+    $(".container").append("<div class='row'><div class='col-2 hour text-right' id='hour" +
+        (index + 9) + "'><span>" + value.format("h A") + "</span></div><div class='col-8 event-group' id='timeblock" +
+        (index + 9) + "'><textarea class='events col-12' id='eventblock" + (index + 9) + "'>" + events[index] + "</textarea></div>" +
+        "<div class='col-2 save-delete' id='save-delete" + (index + 9) + "'><i class='fas fa-save' title='Save Event'></i> <i class='fas fa-trash' title='Remove Event'></i></div></div></div>");
+});
+
+$("#currentDay").text(currentDay);
